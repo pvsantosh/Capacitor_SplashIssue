9067582e2776ad8ff593f0a55e6dad823f7e6b18
santosh <pvsantosh@me.com>
2021-08-27 14:04:37 -0400

    repo setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { SplashScreen } from '@capacitor/splash-screen'
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.initApp();
+  }
+  initApp() {
+    SplashScreen.hide();
+  }
 }
